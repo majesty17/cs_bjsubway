2024157487fb6be7d659dac07ad2977d504c204b
liuhongwei01 <liuhongwei01@baidu.com>
2022-01-21 20:33:03 +0800

    first commit

---
+++ cs_bjsubway/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LitJson;
+
+namespace cs_bjsubway
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            JsonData data = Util.get_subway_data(131, "json");
+            foreach (JsonData line in data["subways"]["l"])
+            {
+                Console.Out.WriteLine(line["l_xmlattr"]["lb"]);
+                foreach (JsonData st in line["p"])
+                {
+                    bool if_st = st["p_xmlattr"]["st"].IsBoolean;
+                    if(if_st)
+                        Console.Out.WriteLine(st["p_xmlattr"]["ln"] + "---------" + st["p_xmlattr"]["lb"]);
+                }
+            }
+        }
+    }
+}
---
+++ cs_bjsubway/Util.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using LitJson;
+
+namespace cs_bjsubway
+{
+    class Util
+    {
+        public static JsonData get_subway_data(int city_id,string format)
+        {
+            string url = string.Format("https://map.baidu.com/?qt=subways&c={0}&format={1}", city_id, format);
+
+            Console.WriteLine(url);
+
+
+            HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            //myRequest.Headers.Add("Accept-Encoding", "gzip, deflate");
+            myRequest.Headers.Add("Accept-Language", "zh-CN,en-US;q=0.8");
+            myRequest.Headers.Add("X-Requested-With", "com.mihoyo.hyperion");
+
+            myRequest.UserAgent = "Mozilla/5.0 (Linux; Android 9; Unspecified Device) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/39.0.0.0 Mobile Safari/537.36 miHoYoBBS/2.2.0";
+            myRequest.Referer = "https://webstatic.mihoyo.com/app/community-game-records/index.html?v=6";
+            myRequest.Accept = "application/json, text/plain, */*";
+
+
+            myRequest.Method = "GET";
+
+            HttpWebResponse myResponse = (HttpWebResponse)myRequest.GetResponse();
+
+            StreamReader reader = new StreamReader(myResponse.GetResponseStream(), Encoding.UTF8);
+
+            string content = reader.ReadToEnd();
+
+            reader.Close();
+
+            //Console.WriteLine(content);
+
+            return JsonMapper.ToObject(content);
+        }
+    }
+}
